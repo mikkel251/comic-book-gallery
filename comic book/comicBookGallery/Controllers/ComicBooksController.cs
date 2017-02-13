2d194297debb227f06a0bf1041aa35045b41fc47
mikk2 <mikk2@DESKTOP-4RUULNF>
2017-02-13 16:47:12 +0100

    design files

--- comic book/comicBookGallery/Controllers/ComicBooksController.cs
+++ comic book/comicBookGallery/Controllers/ComicBooksController.cs
@@ -11,7 +11,21 @@ namespace comicBookGallery.Controllers
 
         public ActionResult Detail()
         {
-            return View(); 
+            ViewBag.SeriesTitle = "The amazing spider-man";
+            ViewBag.IssueNumber = 700;
+            ViewBag.Description = "Lorem ipsum dolor sit amet,sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,  At vero eos et accusam et justo duo dolores et ea rebum.  Lorem ipsum dolor sit amet,  no sea takimata sanctus est Lorem ipsum dolor sit amet.  Stet clita kasd gubergren,  no sea takimata sanctus est Lorem ipsum dolor sit amet.  no sea takimata sanctus est Lorem ipsum dolor sit amet.  no sea takimata sanctus est Lorem ipsum dolor sit amet.  sed diam voluptua. ";
+            ViewBag.Artist = new string[]
+            {
+                "Script: Dan Slott",
+                "Pencils: Humberto Ramos",
+                "Inks: Victor Olazaba",
+                "Colors:Edgar Delgardo",
+                "Letters: Chris Eliopoulos"
+            };
+
+
+
+            return View();
         }
     }
 }
