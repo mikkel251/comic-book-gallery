6b5602f80db3cc5a0de21663b5083d535f5c32ba
mikk2 <mikk2@DESKTOP-4RUULNF>
2017-02-14 12:11:54 +0100

    update comic book model

--- comic book/comicBookGallery/Controllers/ComicBooksController.cs
+++ comic book/comicBookGallery/Controllers/ComicBooksController.cs
@@ -13,7 +13,7 @@ namespace comicBookGallery.Controllers
         {
             ViewBag.SeriesTitle = "The amazing spider-man";
             ViewBag.IssueNumber = 700;
-            ViewBag.Description = "Lorem ipsum dolor sit amet,sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,  At vero eos et accusam et justo duo dolores et ea rebum.  Lorem ipsum dolor sit amet,  no sea takimata sanctus est Lorem ipsum dolor sit amet.  Stet clita kasd gubergren,  no sea takimata sanctus est Lorem ipsum dolor sit amet.  no sea takimata sanctus est Lorem ipsum dolor sit amet.  no sea takimata sanctus est Lorem ipsum dolor sit amet.  sed diam voluptua. ";
+            ViewBag.Description = "<p>Lorem ipsum dolor sit amet,sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,  At vero eos et accusam et justo duo dolores et ea rebum.  Lorem ipsum dolor sit amet,  no sea takimata sanctus est Lorem ipsum dolor sit amet.  Stet clita kasd gubergren,  no sea takimata sanctus est Lorem ipsum dolor sit amet.  no sea takimata sanctus est Lorem ipsum dolor sit amet.  no sea takimata sanctus est Lorem ipsum dolor sit amet.  sed diam voluptua.</p>";
             ViewBag.Artist = new string[]
             {
                 "Script: Dan Slott",
--- comic book/comicBookGallery/Models/ComicBook.cs
+++ comic book/comicBookGallery/Models/ComicBook.cs
@@ -12,9 +12,26 @@ namespace comicBookGallery.Models
         public int IssueNumber { get; set; }
 
         public string DescriptionHtml { get; set; }
-        public Artists [] Artists { get; set; }
+        public Artists[] Artists { get; set; }
         public bool Favorite { get; set; }
 
+        public string DisplayText
+        {
+            get
+            {
+                return SeriesTitle + " #" + IssueNumber;
+            }
 
+        }
+
+        //Series-title-issuenumber.jpg
+        //billede til den fejl der vises
+        public string CoverImageFileName
+        {
+            get
+            {
+                return SeriesTitle.Replace(" ", "-").ToLower() + "-" + ".jpg";
+            }
+        } 
     }
 }
