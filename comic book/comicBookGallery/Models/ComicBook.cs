2be1ba388f76cd193110b4007dad9428c3a1242b
mikk2 <mikk2@DESKTOP-4RUULNF>
2017-02-14 12:03:47 +0100

    Added comic book and artists models

---
+++ comic book/comicBookGallery/Models/ComicBook.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace comicBookGallery.Models
+{
+    public class ComicBook
+    {
+        public int id { get; set; }
+        public string SeriesTitle { get; set; }
+        public int IssueNumber { get; set; }
+
+        public string DescriptionHtml { get; set; }
+        public Artists [] Artists { get; set; }
+        public bool Favorite { get; set; }
+
+
+    }
+}
